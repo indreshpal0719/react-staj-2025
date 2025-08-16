8227d68392ca21a026b81c3273e9edb6617f6293
Zafer AYAN <ozcanzaferayan@gmail.com>
2025-08-16 23:34:58 +0300

    Add todo list

---
+++ 11-todo-list/src/App.tsx
@@ -0,0 +1,19 @@
+// rafce
+
+import BottomNavigation from "./components/BottomNavigation";
+import Navbar from "./components/Navbar";
+import TaskList from "./components/TaskList";
+import TomorrowPreview from "./components/TomorrowPreview";
+
+const App = () => {
+  return (
+    <div className="bg-orange-50 pb-24">
+      <Navbar />
+      <TaskList />
+      <TomorrowPreview />
+      <BottomNavigation />
+    </div>
+  );
+};
+
+export default App;
